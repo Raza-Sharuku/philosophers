7043bf8c197c9b9b6cf39c1a69eb24f3c8155e9f
Shahrukh Raza <shahrukh4062@gmail.com>
2023-08-17 20:30:41 +0900

    raza

--- philosophers.c
+++ philosophers.c
@@ -6,8 +6,33 @@
 /*   By: razasharuku <razasharuku@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 18:11:24 by razasharuku       #+#    #+#             */
-/*   Updated: 2023/08/12 18:11:51 by razasharuku      ###   ########.fr       */
+/*   Updated: 2023/08/17 11:36:57 by razasharuku      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"philosophers.h"
+
+int	argc_error(void)
+{
+	printf("USAGE: ./philo Number_of_philosophers Time_to_die Time_to_eat \
+	Time_to_sleep [ Number_of_times_each_philosopher_must_eat ] \n");
+	return (1);
+}
+
+t_philo *get_philo_values(char **argv)
+{
+	t_philo *philo;
+}
+
+int main(int argc, char **argv)
+{
+	struct timeval *tv;
+	t_philo *philo;
+
+	if (argc != 5 && argc != 6)
+		return (argc_error());
+	philo = get_philo_values(argv);
+	gettimeofday(tv, NULL);
+	printf("%ld %06d\n", tv->tv_sec, tv->tv_usec);
+	return (0);
+}
--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,37 @@
 /*   By: razasharuku <razasharuku@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/12 18:12:01 by razasharuku       #+#    #+#             */
-/*   Updated: 2023/08/12 18:12:03 by razasharuku      ###   ########.fr       */
+/*   Updated: 2023/08/17 11:32:45 by razasharuku      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <sys/time.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+#   define TIMEVAL_TO_TIMESPEC(tv, ts) { \
+		(ts)->tv_sec = (tv)->tv_sec; \
+		(ts)->tv_nsec = (tv)->tv_usec * 1000; \
+}
+
+typedef struct s_timeval
+{
+	time_t tv_sec;
+	suseconds_t tv_usec;
+}			t_timeval;
+
+typedef struct s_philo
+{
+	int	Number_of_philosophers;
+	int	Time_to_die;
+	int	Time_to_eat;
+	int	Time_to_sleep;
+	int	Num_of_times_each_philo_must_eat;
+}				t_philo;
+
+
+#endif
